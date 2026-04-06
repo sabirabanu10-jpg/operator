cf2f59680f14fc08adb5c2dc793acb6c020ccda5
sabirabanu10-jpg <sabirabanu10@gmail.com>
2026-04-06 17:16:33 +0400

    opp

---
+++ Arr1.java
@@ -0,0 +1,11 @@
+class Arr1
+{
+	public static  void main (String[] args)
+	{
+		int a=10;
+		a+= 10;
+		a-=40;
+		System.out.println(a);
+	}
+	
+}
---
+++ Relate.java
@@ -0,0 +1,16 @@
+class Relate
+{
+	public static void main(String [] args)
+	{
+		int a=10;
+		int b=20;
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a>=b);
+		System.out.println(a<=b);
+		System.out.println(a==b);
+		System.out.println(a!=b);
+		
+		
+	}
+}
